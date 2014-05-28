35d1bf17f68fdf41997743522377babcfd6581e8
Haluk Demir <web@halukdemir.com>
2014-05-28 23:45:13 +0300

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'c2betapp',
 			'username'  => 'c2promosos',
-			'password'  => 'mavi1233',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
